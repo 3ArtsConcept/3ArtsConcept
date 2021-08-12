c79653302ee885743a83fa944e7da79623b06b21
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-08-12 09:45:52 +0700

    thats all

--- pages/index.vue
+++ pages/index.vue
@@ -205,26 +205,6 @@
           </p>
           <!-- <button class="btn btn_contact">See More</button> -->
         </div>
-        <!-- START Modal for product -->
-        <b-modal size="lg" id="modal-1" v-model="productModal" hide-footer>
-          <template #modal-title>
-            <div class="modal-header">
-              <!-- <button
-                type="button"
-                @click="productModal = !productModal"
-                class="close"
-              >
-                &times;
-              </button> -->
-              <h4 class="modal-title">{{ header }}</h4>
-            </div>
-          </template>
-          <div class="modal-body">
-            <b-img :src="image" :alt="alt" rounded="circle"></b-img>
-            <p v-html="content" class="mt-4"></p>
-          </div>
-        </b-modal>
-        <!-- END Modal for product -->
 
         <div class="col-md-7">
           <div class="row no-gutter product_wrap_50">
@@ -423,6 +403,27 @@
         </footer>
       </div>
     </div>
+    <!-- START Modal for product -->
+    <b-modal size="lg" id="modal-1" v-model="productModal" hide-footer>
+      <template #modal-title>
+        <div class="modal-header">
+          <!-- <button
+                type="button"
+                @click="productModal = !productModal"
+                class="close"
+              >
+                &times;
+              </button> -->
+          <h4 class="modal-title">{{ header }}</h4>
+        </div>
+      </template>
+      <div class="modal-body">
+        <b-img :src="image" :alt="alt" rounded="circle"></b-img>
+        <p v-html="content" class="mt-4"></p>
+      </div>
+    </b-modal>
+    <!-- END Modal for product -->
+
     <!-- START Modal for partner -->
     <b-modal
       size="xl"
@@ -918,7 +919,7 @@ export default {
         number: '',
         message: '',
       },
-
+      type: '',
       error: {
         Name: false,
         Email: false,
@@ -942,30 +943,52 @@ export default {
       return this.type + 'Modal'
     },
     alt() {
-      if (this.type == 'wedding') return '3Arts Wedding Category Popup Image'
-      else if (this.type == 'child') return '3Arts Young Ones Popup Image'
-      else if (this.type == 'portrait')
+      if (this.type == 'wedding') {
+        return '3Arts Wedding Category Popup Image'
+      }
+      if (this.type == 'child') {
+        return '3Arts Young Ones Popup Image'
+      }
+      if (this.type == 'portrait') {
         return '3Arts Individual Portrait Popup Image'
-      return '3Arts Corportate Branding Category Popup Image'
+      } else {
+        return '3Arts Corportate Branding Category Popup Image'
+      }
     },
     header() {
-      if (this.type == 'wedding') return 'Wedding'
-      else if (this.type == 'child') return 'Young Ones'
-      else if (this.type == 'portrait') return 'Individual Portrait'
-      return 'Corporate Branding'
+      if (this.type == 'wedding') {
+        return 'Wedding'
+      }
+      if (this.type == 'child') {
+        return 'Young Ones'
+      }
+      if (this.type == 'portrait') {
+        return 'Individual Portrait'
+      } else {
+        return 'Corporate Branding'
+      }
     },
     image() {
-      if (this.type == 'wedding') return '/img/two-thumbs.jpg'
-      else if (this.type == 'child') return '/img/child.jpg'
-      else if (this.type == 'portrait') return '/img/graduant.jpg'
-      return '/img/man-woman.jpg'
+      if (this.type == 'wedding') {
+        return '/img/two-thumbs.jpg'
+      }
+      if (this.type == 'child') {
+        return '/img/child.jpg'
+      }
+      if (this.type == 'portrait') {
+        return '/img/graduant.jpg'
+      } else {
+        return '/img/man-woman.jpg'
+      }
     },
     content() {
       if (this.type == 'wedding') {
         return '<p> Walking down the aisle hand in hand is blissful, embarking this new chapter in life is challenging, taking the leap of faith takes courage!<br><br> Keep all of these memories together in just one tailor-made showcase meant for just the both of you to reminisce as the years go by!</p>'
-      } else if (this.type == 'child') {
+      }
+      if (this.type == 'child') {
         return '<p>Children, the innocence and happiness in them goes by much quicker than what we perceive. They are your one and only in the world, capture that one impression of them and it stays.<br><br>Photographs show just a picture; a showcase creates the sense of belonging for your family!</p>'
-      } else if (this.type == 'portrait') {
+      }
+      if (this.type == 'portrait') {
         return '<p>“You Only Live Once!” Time to take a step back, it’s time to credit yourself.<br><br>Life’s a quick journey, only if you mark your own checkpoints and celebrate them!<br><br>Achievements not to be forgotten, recognition not to be neglected and significant moments accounted for brings value to you!</p>'
       } else {
         return '<p>Branding with an identity marks its position with an indirect edge over competitors for all corporate and businesses.<br><br> Create the uptrend symbolic icon of your products/services for greater values enhanced onto your business.</p>'
@@ -998,8 +1021,8 @@ export default {
     },
     showModal(category) {
       this.productModal = !this.productModal
-      console.log(this.productModal)
       this.type = category
+      console.log(category)
     },
     showProductModal() {
       this.partnerModal = !this.productModal
